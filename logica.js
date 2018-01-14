51fbb2f0b2d5acf6396d5b6e2c869dcd4e71fd78
Padovese <bhspadoveze@gmail.com>
2018-01-14 17:11:59 -0200

    Uploading css e js files

---
+++ logica.js
@@ -0,0 +1,14 @@
+function soma(n1, n2){
+			document.write(n1+n2);
+		}
+
+function tabuada(numeroRecebido){
+	document.write("Número recebido: " + numeroRecebido);
+	document.write("<br>");
+
+	for(i = 1; i <= 10; i++){
+		document.write(numeroRecebido + " x " + i + " = " + numeroRecebido*i);
+		document.write("<br>");
+	}
+
+}
